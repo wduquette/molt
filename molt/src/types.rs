44f9d543f24bcb7f6cc0947b835baf9cbe4bf0bc
Will Duquette <will@wjduquette.com>
2019-03-21 09:18:11 -0700

    Added documentation for the types.rs module

--- molt/src/types.rs
+++ molt/src/types.rs
@@ -5,16 +5,87 @@ use crate::interp::Interp;
 // Molt Numeric Types
 
 /// The standard integer type for Molt code.
-pub type MoltInt   = i64;
+///
+/// The interpreter uses this type internally for all Molt integer values.
+/// The primary reason for defining this as a type alias is future-proofing: at
+/// some point we may wish to replace `MoltInt` with a more powerful type that
+/// supports BigNums, or switch to `i128`.
+pub type MoltInt = i64;
 
 /// The standard floating point type for Molt code.
+///
+/// The interpreter uses this type internally for all Molt floating-pojint values.
+/// The primary reason for defining this as a type alias is future-proofing: at
+/// some point we may wish to replace `MoltFloat` with a more powerful type that
+/// supports BigNums, or switch to `f128`.
 pub type MoltFloat = f64;
 
 /// The standard list type for Molt code.
+///
+/// Lists are an important data structure, both in Molt code proper and in Rust code
+/// that implements and works with Molt commands.  The standard internal representation
+/// for Molt values is `String`, so the natural representation for lists is currently
+/// `Vec<String>`.  In time, though, `String` will be replaced by `MoltValue`, a struct
+/// that will maintain a string representation, an internal representation, or both,
+/// for the sake of efficiency.  At that time `MoltList` will be changed to (something like)
+/// `Vec<MoltValue>`...or, possibly, to a struct that contains such a vector.
 pub type MoltList = Vec<String>;
 
-/// The result of calling a function during Molt script evaluation, other than
-/// `Ok(str)`.
+/// Molt's standard `Result<T,E>` type.
+///
+/// This is the most common result value returned by Molt code.  The
+/// `Ok` type is `String`, the standard Molt value type; the `Err` type is
+/// [`ResultCode`], which encompasses the four exceptional Molt return values.
+///
+/// # Future Work
+///
+/// * Eventually the normal `String` result will be replaced by a `MoltValue` type
+///   for efficiency.
+///
+/// [`ResultCode`]: enum.ResultCode.html
+pub type MoltResult = Result<String, ResultCode>;
+
+/// Exceptional results of evaluating a Molt script.
+///
+/// A Molt script can return a normal result, as indicated by the `Ok`
+/// [`MoltResult`], or it can return one of a number of exceptional results, which
+/// will bubble up the call stack in the usual way until caught.
+///
+/// * `Error(String)`: This code indicates a Molt error; the string is the error message
+///   for display to the user.
+///
+/// * `Return(String)`: This code indicates that a Molt procedured called the
+///   `return` command.  The string is the returned value, or the empty string if
+///   no value was returned.  This result will bubble up until it reaches the top-level
+///   of the procedure, which will then return the value as a normal `Ok` result.  If
+///   it is received when evaluating an arbitrary script, i.e., if `return` is called outside
+///   of any procedure, the interpreter will convert it into a normal `Ok` result.
+///
+/// * `Break`: This code indicates that the Molt `break` command was called.  It will
+///   break out of the inmost enclosing loop in the usual way.  When returned outside a
+///   loop (or some user-defined control structure that supports `break`), the interpreter
+///   will convert it into an error.
+///
+/// * `Continue`: This code indicates that the Molt `continue` command was called.  It will
+///   continue with the next iteration of the inmost enclosing loop in the usual way.
+///   When returned outside a loop (or some user-defined control structure that supports
+///   `continue`), the interpreter will convert it into an error.
+///
+/// Client code will usually see only the `Error` code; the others will most often be caught
+/// and handled within the interpreter.
+///
+/// # Future Work
+///
+/// * The `Return` code returns a `String`, this being the standard Molt value representation.
+///   In time we will replace `String` with `MoltValue`, a more complex type that will support
+///   both string and internal representations of data.
+///
+/// * Standard TCL allows for an arbitrary number of result codes, which in turn allows the
+///   application to define an arbitrary number of new kinds of control structures that are
+///   distinct from the standard ones.  At some point we might wish to add one or more
+///   generic result codes, parallel to `Break` and `Continue`, for this purpose.
+///
+/// [`MoltResult`]: type.MoltResult.html
 #[derive(Eq, PartialEq, Clone, Debug)]
 pub enum ResultCode {
     Error(String),
@@ -24,6 +95,7 @@ pub enum ResultCode {
 }
 
 impl ResultCode {
+    /// Indicates whether the result code is an `Error(String)`.
     pub fn is_error(&self) -> bool {
         match self {
             ResultCode::Error(_) => true,
@@ -32,18 +104,40 @@ impl ResultCode {
     }
 }
 
-pub type MoltResult = Result<String, ResultCode>;
-
-/// A simple command function, used to implement a command without any attached
-/// context data.
-pub type CommandFunc = fn(&mut Interp, &[&str]) -> MoltResult;
-
-/// A trait defining a command object: a struct that implements a command (and may also
+/// A trait defining a Molt command object: a struct that implements a command (and may also
 /// have context data).
+///
+/// A simple command should be defined as a [`CommandFunc`]; define a full-fledged `Command`
+/// struct when the command needs access to context data other than that provided by the
+/// the interpreter itself.  For example, application-specific commands will often need
+/// access to application data, which can be provided as attributes of the `Command`
+/// struct.
+///
+/// [`CommandFunc`]: type.CommandFunc.html
 pub trait Command {
+    /// The `Command`'s execution method: the Molt interpreter calls this method  to
+    /// execute the command.  The method receives the object itself, the interpreter,
+    /// and an array representing the command and its arguments.
     fn execute(&self, interp: &mut Interp, argv: &[&str]) -> MoltResult;
 }
 
+
+/// A simple command function, used to implement a command without any attached
+/// context data (other than the [`Interp`] itself).
+///
+/// The command function receives the interpreter and an array representing the
+/// command and its arguments.
+///
+/// [`Interp`]: ../interp/struct.Interp.html
+pub type CommandFunc = fn(&mut Interp, &[&str]) -> MoltResult;
+
 /// Used for defining subcommands of ensemble commands.
-/// Full description TODO, because it might change.
+///
+/// The tuple fields are the subcommand's name and [`CommandFunc`].
+///
+/// **Note:** This interface isn't yet stable; we may want to support
+/// [`Command`] instead of [`CommandFunc`].
+///
+/// [`Command`]: trait.Command.html
+/// [`CommandFunc`]: type.CommandFunc.html
 pub struct Subcommand(pub &'static str, pub CommandFunc);

3876a9bd04346d7a659683217cf3cb4110b4a324
Will Duquette <will@wjduquette.com>
2020-04-04 10:16:13 -0700

    Initial types.rs scrub.

--- molt/src/types.rs
+++ molt/src/types.rs
@@ -14,7 +14,7 @@
 //! dictionaries.
 //!
 //! [`MoltResult`]: type.MoltResult.html
-//! [`Exception`]: type.Exception.html
+//! [`Exception`]: struct.Exception.html
 //! [`MoltInt`]: type.MoltInt.html
 //! [`MoltFloat`]: type.MoltFloat.html
 //! [`MoltList`]: type.MoltList.html
@@ -61,17 +61,20 @@ pub type MoltDict = IndexMap<Value, Value>;
 /// The standard `Result<T,E>` type for Molt code.
 ///
 /// This is the return value of all Molt commands, and the most common return value
-/// throughout the Molt code base.  Every Molt command returns a [`Value`] on success;
-/// if the command has no explicit return value, it returns the empty `Value`, a `Value`
-/// whose string representation is the empty string.
+/// throughout the Molt code base.  Every Molt command returns a [`Value`] (i.e., `Ok(Value)`)
+/// on success; if the command has no explicit return value, it returns the empty
+/// `Value`, a `Value` whose string representation is the empty string.
 ///
-/// A Molt command returns an [`Exception`] whenever the calling Molt script should
-/// return early: on error, when returning an explicit result via the `return` command,
-/// or when breaking out of a loop via the `break` or `continue` commands.  The precise
-/// nature of the return is indicated by the [`Exception`]'s [`ResultCode`].
+/// A Molt command returns an [`Exception`] (i.e., `Err(Exception)`) whenever the calling Molt
+/// script should return early: on error, when returning an explicit result via the
+/// `return` command, or when breaking out of a loop via the `break` or `continue`
+/// commands.  The precise nature of the return is indicated by the [`Exception`]'s
+/// [`ResultCode`].
 ///
 /// Many of the functions in Molt's Rust API also return `MoltResult`, for easy use within
-/// Molt command definitions.
+/// Molt command definitions. Others return `Result<T,Exception>` for some type `T`; these
+/// are intended to produce a `T` value in Molt command definitions, while easily propagating
+/// errors up the call chain.
 ///
 /// [`Exception`]: struct.Exception.html
 /// [`ResultCode`]: enum.ResultCode.html
@@ -81,52 +84,71 @@ pub type MoltResult = Result<Value, Exception>;
 /// This enum represents the different kinds of [`Exception`] that result from
 /// evaluating a Molt script.
 ///
-/// Client code will usually see only the `Error` code; the others will most often be caught
-/// and handled within the interpreter.  However, client code may explicitly catch and handle
-/// the `Return`, `Break`, and `Continue` codes at both the Rust and the TCL level
-/// (see the `catch` command) in order to implement application-specific control structures.
-///
-/// # Future Work
-///
-/// * Standard TCL allows for an arbitrary number of result codes, which in turn allows the
-///   application to define an arbitrary number of new kinds of control structures that are
-///   distinct from the standard ones.  This type provides the `Other` code for this
-///   purpose; however, there is as yet no support for it in either the Rust or TCL APIs.
+/// Client Rust code will usually see only the `Error` code; the others will most often be
+/// caught and handled within the interpreter.  However, client code may explicitly catch
+/// and handle `Break` and `Continue` (or application-defined codes) at both the Rust and
+/// the TCL level in order to implement application-specific control structures.  (See
+/// The Molt Book on the `return` and `catch` commands for more details on the TCL
+/// interface.)
 ///
 /// [`Exception`]: struct.Exception.html
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum ResultCode {
-    /// Value for `return -code` to indicate returning `Ok(value)` higher up the stack.
+    /// Value for `return -code` to indicate returning an `Ok(value)` higher up the stack.
+    /// Client code should rarely if ever need to refer to this constant explicitly.
     Okay,
 
-    /// A Molt error.  The `Exception::value` is the error message for display to the user.
+    /// A Molt error.  The `Exception::value` is the error message for display to the
+    /// user.  The [`molt_err`] macro is usually used to produce errors in client
+    /// code; but the [`Exception`] struct has a number of methods that given finer
+    /// grained control.
+    ///
+    /// [`molt_err`]: ../macro.molt_err.html
+    /// [`Exception`]: struct.Exception.html
     Error,
 
     /// An explicit return from a Molt procedure.  The `Exception::value` is the returned
     /// value, or the empty value if `return` was called without a return value.  This result
-    /// will bubble up until it reaches the top-level of the enclosing procedure, which will
-    /// then return the value as a normal `Ok` result.  If it is received when evaluating an
-    /// arbitrary script, i.e., if `return` is called outside of any procedure, the
+    /// will bubble up through one or more stack levels (i.e., enclosing TCL procedure calls)
+    /// and then yield the value as a normal `Ok` result.  If it is received when evaluating
+    /// an arbitrary script, i.e., if `return` is called outside of any procedure, the
     /// interpreter will convert it into a normal `Ok` result.
+    ///
+    /// Clients will rarely need to interact with or reference this result code
+    /// explicitly, unless implementing application-specific control structures.  See
+    /// The Molt Book documentation for the `return` and `catch` command for the semantics.
     Return,
 
     /// A `break` in a Molt loop.  It will break out of the inmost enclosing loop in the usual
     /// way.  If it is returned outside a loop (or some user-defined control structure that
-    /// supports `break`), the interpreter will convert it into an error.
+    /// supports `break`), the interpreter will convert it into an `Error`.
+    ///
+    /// Clients will rarely need to interact with or reference this result code
+    /// explicitly, unless implementing application-specific control structures.  See
+    /// The Molt Book documentation for the `return` and `catch` command for the semantics.
     Break,
 
     /// A `continue` in a Molt loop.  Execution will continue with the next iteration of
     /// the inmost enclosing loop in the usual way.  If it is returned outside a loop (or
     /// some user-defined control structure that supports `break`), the interpreter will
     /// convert it into an error.
+    ///
+    /// Clients will rarely need to interact with or reference this result code
+    /// explicitly, unless implementing application-specific control structures.  See
+    /// The Molt Book documentation for the `return` and `catch` command for the semantics.
     Continue,
 
-    /// Experimental; not in use yet.
+    /// A mechanism for defining application-specific result codes.
+    /// Clients will rarely need to interact with or reference this result code
+    /// explicitly, unless implementing application-specific control structures. See
+    /// The Molt Book documentation for the `return` and `catch` command for the semantics.
     Other(MoltInt),
 }
 
 impl fmt::Display for ResultCode {
+    /// Formats a result code for use with the `return` command's `-code` option.
+    /// This is part of making `ResultCode` a valid external type for use with `Value`.
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             ResultCode::Okay => write!(f, "ok"),
@@ -142,6 +164,8 @@ impl fmt::Display for ResultCode {
 impl FromStr for ResultCode {
     type Err = String;
 
+    /// Converts a symbolic or numeric result code into a `ResultCode`.  This is part
+    /// of making `ResultCode` a valid external type for use with `Value`.
     fn from_str(value: &str) -> Result<Self, Self::Err> {
         match value {
             "ok" => return Ok(ResultCode::Okay),
@@ -169,8 +193,13 @@ impl FromStr for ResultCode {
 }
 
 impl ResultCode {
-    /// A convenience: retrieves the enumerated value, converting it from
+    /// A convenience: retrieves a result code string from the input `Value`
+    /// the enumerated value as an external type, converting it from
     /// `Option<ResultCode>` into `Result<ResultCode,Exception>`.
+    ///
+    /// This is primarily intended for use by the `return` command; if you really
+    /// need it, you'd best be familiar with the implementation of `return` in
+    /// `command.rs`, as well as a good bit of `interp.rs`.
     pub fn from_value(value: &Value) -> Result<Self, Exception> {
         if let Some(x) = value.as_copy::<ResultCode>() {
             Ok(x)
@@ -179,7 +208,9 @@ impl ResultCode {
         }
     }
 
-    /// Return the result code as an integer.
+    /// Returns the result code as an integer.
+    ///
+    /// This is primarily intended for use by the `catch` command.
     pub fn as_int(&self) -> MoltInt {
         match self {
             ResultCode::Okay => 0,
@@ -192,22 +223,20 @@ impl ResultCode {
     }
 }
 
-/// This enum represents the exceptional results of evaluating a Molt script, as
+/// This struct represents the exceptional results of evaluating a Molt script, as
 /// used in [`MoltResult`].  It is often used as the `Err` type for other
 /// functions in the Molt API, so that these functions can easily return errors when used
 /// in the definition of Molt commands.
 ///
-/// A Molt script can return a normal result, as indicated by [`MoltResult`]'s `Ok`
-/// variant, or it can return one of a number of exceptional results, which
-/// will bubble up the call stack in the usual way until caught.  The different kinds of
-/// exceptional result are defined by the [`ResultCode`] enum.
-///
-/// # Future Work
-///
-/// * Accumulate the error stack trace (the `errorInfo`) as error exceptions bubble up.
-/// * Accept and retain the error code (the `errorCode`)
-/// * Support the `return` command's options
-/// * Support the `catch` command's `optionVarName`.
+/// A Molt command or script can return a normal result, as indicated by
+/// [`MoltResult`]'s `Ok` variant, or it can return one of a number of exceptional results via
+/// `Err(Exception)`.  Exceptions bubble up the call stack in the usual way until
+/// caught. The different kinds of exceptional result are defined by the
+/// [`ResultCode`] enum.  Client code is primarily concerned with `ResultCode::Error`
+/// exceptions; other exceptions are handled by the interpreter and various control
+/// structure commands.  Except within application-specific control structure code (a rare
+/// bird), non-error exceptions can usually be ignored or converted to error exceptions—
+/// and the latter is usually done for you by the interpreter anyway.
 ///
 /// [`ResultCode`]: enum.ResultCode.html
 /// [`MoltResult`]: type.MoltResult.html
@@ -231,7 +260,12 @@ pub struct Exception {
 }
 
 impl Exception {
-    /// Creates an `Error` exception with the given error message.
+    /// Creates an `Error` exception with the given error message.  This is primarily
+    /// intended for use by the [`molt_err!`] macro, but it can also be used directly.
+    ///
+    /// TODO: add example.
+    ///
+    /// [`molt_err`]: ../macro.molt_err.html
     pub fn molt_err(msg: Value) -> Self {
         let data = ErrorData::new(Value::from("NONE"), msg.as_str());
 
@@ -244,7 +278,13 @@ impl Exception {
         }
     }
 
-    /// Creates an `Error` exception with the given code and error message.
+    /// Creates an `Error` exception with the given error code and message.  An
+    /// error code is a `MoltList` that indicates the nature of the error.  Standard TCL
+    /// uses the error code to flag specific arithmetic and I/O errors; most other
+    /// errors have the code `NONE`.  At present Molt doesn't define any error codes
+    /// other than `NONE`, so this method is primarily for use by the `throw` command
+    ///
+    /// TODO: add example.
     pub fn molt_err2(error_code: Value, msg: Value) -> Self {
         let data = ErrorData::new(error_code, msg.as_str());
 
@@ -257,7 +297,12 @@ impl Exception {
         }
     }
 
-    /// Creates an `Error` exception with the given data
+    /// Creates an `Error` exception with the given data.
+    ///
+    /// This method is primarily for use by the `return` command, and should rarely if
+    /// ever be needed in client code.  If you fully understand the semantics of the `return` and
+    /// `catch` commands, you'll understand what this does and when you would want
+    /// to use it.  If you don't, you almost certainly don't need it.
     pub fn molt_err3(msg: Value, level: usize, error_code: Option<Value>, error_info: Option<Value>) -> Self {
         let error_code = error_code.unwrap_or_else(|| Value::from("NONE"));
         let error_info = error_info.unwrap_or_else(Value::empty);
@@ -275,8 +320,12 @@ impl Exception {
 
     /// Creates a `Return` exception, with the given return value.  Return `Value::empty()`
     /// if there is no specific result.
+    ///
+    /// This method is primarily for use by the `return` command, and should rarely if
+    /// ever be needed in client code.  If you fully understand the semantics of the `return` and
+    /// `catch` commands, you'll understand what this does and when you would want
+    /// to use it.  If you don't, you almost certainly don't need it.
     pub fn molt_return(value: Value) -> Self {
-
         Self {
             code: ResultCode::Return,
             value,
@@ -289,10 +338,13 @@ impl Exception {
     /// Creates an extended `Return` exception with the given return value, `-level`,
     /// and `-code`. Return `Value::empty()` if there is no specific result.
     ///
-    /// This function is primarily intended for use by the `return` command.
-    ///
     /// It's an error if level == 0 and next_code == Okay; that's
     /// `Ok(value)` rather than an exception.
+    ///
+    /// This method is primarily for use by the `return` command, and should rarely if
+    /// ever be needed in client code.  If you fully understand the semantics of the `return` and
+    /// `catch` commands, you'll understand what this does and when you would want
+    /// to use it.  If you don't, you almost certainly don't need it.
     pub(crate) fn molt_return_ext(value: Value, level: usize, next_code: ResultCode) -> Self {
         assert!(level > 0 || next_code != ResultCode::Okay);
 
@@ -306,6 +358,11 @@ impl Exception {
     }
 
     /// Creates a `Break` exception.
+    ///
+    /// This method is primarily for use by the `break` command, and should rarely if
+    /// ever be needed in client code.  If you fully understand the semantics of the `return` and
+    /// `catch` commands, you'll understand what this does and when you would want
+    /// to use it.  If you don't, you almost certainly don't need it.
     pub fn molt_break() -> Self {
         Self {
             code: ResultCode::Break,
@@ -317,6 +374,11 @@ impl Exception {
     }
 
     /// Creates a `Continue` exception.
+    ///
+    /// This method is primarily for use by the `continue` command, and should rarely if
+    /// ever be needed in client code.  If you fully understand the semantics of the `return` and
+    /// `catch` commands, you'll understand what this does and when you would want
+    /// to use it.  If you don't, you almost certainly don't need it.
     pub fn molt_continue() -> Self {
         Self {
             code: ResultCode::Continue,
@@ -342,37 +404,58 @@ impl Exception {
         }
     }
 
-    /// Returns true if the exception is an error exception, and false otherwise.
+    /// Returns true if the exception is an error exception, and false otherwise.  In client
+    /// code, it almost always will be; and unless you're implementing an application-specific
+    /// control structure can be treated as an error in any event.
+    ///
+    /// TODO: Add example
     pub fn is_error(&self) -> bool {
         self.code == ResultCode::Error
     }
 
     /// Gets the exception's result code.
+    ///
+    /// TODO: Add example
     pub fn code(&self) -> ResultCode {
         self.code
     }
 
-    /// Gets the exception's next code (when `code == ResultCode::Return` only)
+    /// Gets the exception's "next" code (when `code == ResultCode::Return` only).  The
+    /// "next" code is set by the `return` command's `-code` option.  See The Molt Book's
+    /// `return` page for the semantics.  Client code should rarely if ever need to refer
+    /// to this.
     pub fn next_code(&self) -> ResultCode {
         self.next_code
     }
 
-    /// Gets the exception's level.
+    /// Gets the exception's level.  The "level" code is set by the `return` command's
+    /// `-level` option.  See The Molt Book's `return` page for the semantics.  Client code
+    /// should rarely if ever need to refer to this.
     pub fn level(&self) -> usize {
         self.level
     }
 
-    /// Gets the exception's value, i.e., the explicit return value or the error message.
+    /// Gets the exception's value, i.e., the explicit return value or the error message.  In
+    /// client code, this will almost always be an error message.
+    ///
+    /// TODO: Add example
     pub fn value(&self) -> Value {
         self.value.clone()
     }
 
-    /// Gets the exception's error data, if any.
+    /// Gets the exception's [`ErrorData`], only when the code is `ResultCode::Error`.  The
+    /// error date contains the error's code and stack trace information.
+    ///
+    /// TODO: Add example.
+    ///
+    /// [`ErrorData`]: struct.ErrorData.html
     pub fn error_data(&self) -> Option<&ErrorData> {
         self.error_data.as_ref()
     }
 
-    pub fn is_new_error(&self) -> bool {
+    /// This is used by the interpreter when accumulating stack trace information.
+    /// See Interp::eval_script.
+    pub(crate) fn is_new_error(&self) -> bool {
         if let Some(data) = &self.error_data {
             data.is_new()
         } else {
@@ -380,7 +463,11 @@ impl Exception {
         }
     }
 
-    /// Adds a line to the exception's error info.
+    /// Adds a line to the exception's error info, i.e., to its human readable stack trace.
+    /// This is for use by command definitions that execute a TCL script and wish to
+    /// add to the stack trace on error as an aid to debugging.
+    ///
+    /// TODO: Example
     ///
     /// # Panics
     ///
@@ -394,6 +481,8 @@ impl Exception {
     }
 }
 
+/// This struct contains the error code and stack trace (i.e., the "error info" string)
+/// for `ResultCode::Error` exceptions.
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct ErrorData {
     /// The error code; defaults to "NONE"
@@ -417,11 +506,13 @@ impl ErrorData {
         self.error_code.clone()
     }
 
-    pub fn is_new(&self) -> bool {
+    /// Whether this has just been created, or the stack trace has been extended.
+    /// TODO: On rethrow, this should be false!
+    pub(crate) fn is_new(&self) -> bool {
         self.stack_trace.len() == 1
     }
 
-    /// Returns the stack trace.
+    /// Returns the human-readable stack trace as a string.
     pub fn error_info(&self) -> Value {
         Value::from(self.stack_trace.join("\n"))
     }
